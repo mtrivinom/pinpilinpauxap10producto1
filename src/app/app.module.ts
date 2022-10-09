22cda4ad3b6b99c55b3cd529ee6b9886477a9bee
cbanciu1603 <cbanciu@uoc.edu>
2022-10-09 19:35:25 +0200

    rehacer estructura proyecto - conforme pdf producto 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CancionComponent } from './cancion/cancion.component';
-import { NavegableComponent } from './navegable/navegable.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CancionComponent,
-    NavegableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
