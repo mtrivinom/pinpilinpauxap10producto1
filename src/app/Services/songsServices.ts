2ee013ae792b91eb4f8288c33d17d29161671005
cbanciu1603 <cbanciu@uoc.edu>
2022-10-09 19:45:08 +0200

    creado servicios canciones

---
+++ src/app/Services/songsServices.ts
@@ -0,0 +1,23 @@
+export class SongsServices{
+  songs = [
+    {
+      id: 1,
+      title: 'Thriller',
+      artist: 'Michael Jackson',
+      year: '1980',
+      album: 'Thriller',
+      genres: ['R&B', 'Pop']
+    },
+    {
+      id: 2,
+      title: 'Rolling in the Deep',
+      artist: 'Adele',
+      year: '2011',
+      album: '21',
+      genres: ['R&B']
+    }
+  ]
+
+  constructor(){}
+
+}
