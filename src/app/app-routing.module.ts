2c664e49b599f0c75c078d0218604bb1a3bd5e90
Paulina Povis <ppovis@uoc.edu>
2022-10-14 09:53:07 +0200

    Se crea el componente songlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SongListComponent } from './song-list/song-list.component';
 import { SongComponent } from './song/song.component';
 
 const routes: Routes = [
-  {
-    path: 'song/:id', component: SongComponent
-  }
+  { path: '',redirectTo:'/songlist', pathMatch:'full'},
+  { path: 'song/:id', component: SongComponent},
+  { path: 'songlist', component: SongListComponent}
 ];
 
 @NgModule({
